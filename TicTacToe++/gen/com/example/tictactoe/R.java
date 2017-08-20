d8f5424a2d0a481ef20f647ba6fc015ea97758c3
rahul <rahul@localhost>
2017-08-21 00:30:42 +0530

    GOD KNOWS
    Signed-off-by: rahul <rahul@localhost>

--- TicTacToe++/gen/com/example/tictactoe/R.java
+++ TicTacToe++/gen/com/example/tictactoe/R.java
@@ -26,7 +26,6 @@ public final class R {
         public static final int orientation=0x7f060005;
         public static final int player1=0x7f060001;
         public static final int player2=0x7f060003;
-        public static final int popup=0x7f06000b;
         public static final int scrollView1=0x7f060004;
         public static final int table=0x7f060007;
         public static final int table2=0x7f060000;
@@ -36,7 +35,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int popup_window=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
